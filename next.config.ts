39ad0dc6fc5ac1a6c731fa936a44493694e64237
Keerthivasan Venkitajalam <cb.sc.u4aie23037@cb.students.amrita.edu>
2025-07-04 01:09:48 +0530

    chore: undo previous addition in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,12 +10,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
     ],
   },
 };
