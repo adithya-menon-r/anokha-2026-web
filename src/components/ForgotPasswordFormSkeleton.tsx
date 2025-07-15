94442869094d2eeec1033eae9dae6908caaffd02
shivanesh1495 <mcshivanesh777@gmail.com>
2025-07-15 20:58:31 +0530

    Requested changes made

--- src/components/ForgotPasswordFormSkeleton.tsx
+++ src/components/ForgotPasswordFormSkeleton.tsx
@@ -4,7 +4,7 @@ import { SkeletonBlock } from '@/components/SkeletonBlock';
 export const ForgotPasswordFormSkeleton: React.FC = () => (
   <div className="forgot-password-skeleton-inner">
     <div className="forgot-password-skeleton-header">
-      <SkeletonBlock className="forgot-password-skeleton-logo" />
+      {/* Removed logo skeleton block */}
       <SkeletonBlock className="forgot-password-skeleton-title" />
       <SkeletonBlock className="forgot-password-skeleton-subtitle" />
     </div>
