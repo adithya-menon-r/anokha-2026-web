a5fea935966d08120d34be941104fe0217853010
shivanesh1495 <mcshivanesh777@gmail.com>
2025-07-17 00:10:14 +0530

    changes made as per request

---
+++ src/components/ResetPasswordForm.tsx
@@ -0,0 +1,5 @@
+export type ResetPasswordFormValues = { password: string };
+export function ResetPasswordForm(props: any) {
+  // Minimal placeholder form
+  return <form>{/* form elements */}</form>;
+} 
---
+++ src/components/ResetPasswordFormSkeleton.tsx
@@ -0,0 +1,3 @@
+export function ResetPasswordFormSkeleton() {
+  return <div>Loading...</div>;
+} 
