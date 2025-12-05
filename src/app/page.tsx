6909498a101eb20c63d6b27b06ae776eaf318c8e
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-12-06 00:20:45 +0530

    added sponsors marquee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import OrbitGallery from '@/components/gallery/OrbitGallery';
 import ParallaxSection from '@/components/ParallaxSection';
+import SponsorsMarquee from '@/components/SponsorsMarquee';
+import UnifiedBackground from '@/components/UnifiedBackground';
 import HeroWarp from '@/features/home/components/HeroWarp';
 
 export default function HomePage() {
   return (
-    <main className="min-h-screen w-full">
-      <HeroWarp />
-      <ParallaxSection />
-      <OrbitGallery />
+    <main className="relative min-h-screen w-full bg-void">
+      <UnifiedBackground />
+      <div className="relative z-10">
+        <HeroWarp />
+        <SponsorsMarquee />
+        <ParallaxSection />
+        <OrbitGallery />
+      </div>
     </main>
   );
 }
