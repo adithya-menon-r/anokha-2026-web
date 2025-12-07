12d1fa6a7796df375a0af521382556317ed8f6a7
Revanth Singothu <revanthsingothu@gmail.com>
2025-12-07 21:44:30 +0000

    ph: removed posthog instrument

---
+++ src/app/providers.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { usePathname, useSearchParams } from 'next/navigation';
+import posthog from 'posthog-js';
+import { PostHogProvider as PHProvider, usePostHog } from 'posthog-js/react';
+import { useEffect } from 'react';
+
+export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host:
+        process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+      person_profiles: 'identified_only',
+      defaults: '2025-11-30',
+    });
+  }, []);
+
+  return <PHProvider client={posthog}>{children}</PHProvider>;
+}
