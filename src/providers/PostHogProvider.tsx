8bb490aebd3feedcd6512171d6469b325b608bd5
Adithya Menon R <adukottan@gmail.com>
2026-01-05 03:27:09 +0530

    fix: revert posthog edit

--- src/providers/PostHogProvider.tsx
+++ src/providers/PostHogProvider.tsx
@@ -6,13 +6,12 @@ import { useEffect } from 'react';
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
-      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-        api_host:
-          process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
-        person_profiles: 'identified_only',
-      });
-    }
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host:
+        process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+      person_profiles: 'identified_only',
+      defaults: '2025-11-30',
+    });
   }, []);
 
   return <PHProvider client={posthog}>{children}</PHProvider>;
